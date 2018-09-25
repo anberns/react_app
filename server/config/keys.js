b096177aa92e8530bd8eb8d6b0d5a769ddaea35e
Aaron Berns <anberns@gmail.com>
2018-09-25 11:56:38 -0400

    server with authentication again

--- server/config/keys.js
+++ server/config/keys.js
@@ -7,5 +7,3 @@ if (process.env.NODE_ENV === 'production') {
     // dev keys, export required keys
     module.exports = require('./dev');
 }
-863239152291-rj6ffa1t6m2l0vkh0q3rb31u47egn79h.apps.googleusercontent.com
-GP4i1k3kK5zJS1tNVwcNLJW9
